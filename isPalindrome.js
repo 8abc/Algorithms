9b30c7c2dd2be30d6a43c15c1340c31dd5a89493
8abc <anadelaine.catalla@gmail.com>
2019-08-27 19:35:05 -0700

    added 2 functions to check if a word is a palindrome

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,12 +1,27 @@
 // Write a function that takes a string and returns true if the string is a palindrome.  It returns false if it is not.
 //javascript
-function isPalindrome(str) {
+isPalindrome = str => {
   return (
     str ==
     str
-      .tolowercase()
       .split("")
       .reverse()
       .join("")
   );
-}
+};
+isPalindrome("racecar"); //returns true
+isPalindrome("good"); //returns false
+
+const isPal2 = str => {
+  for (let i = 0; i <= str.length / 2; i++) {
+    //added the /2 here because you only need to go half way through the string, since you're comparing both sides towards the middle :)
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log("cat should be false", isPal2("cat"));
+console.log("mom should be true", isPal2("mom"));
+console.log("racecar should be true", isPal2("racecar"));
